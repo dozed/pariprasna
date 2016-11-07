39fbf78cccaa4ef93fb3f793815162e96fcb2ab6
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-11-07 16:03:48 +0100

    Add picture claim

--- src/main/scala/pariprasna/UserInfoEndpoint.scala
+++ src/main/scala/pariprasna/UserInfoEndpoint.scala
@@ -1,10 +1,31 @@
 package pariprasna
 
+import cats.data.Xor
 import io.circe._
 import org.http4s._
-import scalaz._, Scalaz._
+
+import scalaz._
+import Scalaz._
 
 object UserInfoEndpoint {
+
+
+  // UserInfo endpoints
+  // http://openid.net/specs/openid-connect-core-1_0.html#UserInfo
+
+  // Google: OpenID Connect
+  // https://developers.google.com/identity/protocols/OpenIDConnect
+  // https://accounts.google.com/.well-known/openid-configuration
+  val googleUserInfoUri = Uri.uri("https://www.googleapis.com/oauth2/v3/userinfo")
+
+  // Google+ API user profile in OpenID Connect-like format
+  // https://developers.google.com/+/web/api/rest/openidconnect/getOpenIdConnect
+  val googlePlusGetOpenIdConnectUri = Uri.uri("https://www.googleapis.com/plus/v1/people/me/openIdConnect")
+
+  // Facebook: proprietary OAuth 2.0 authentication extension
+  val facebookUserInfoUri = Uri.uri("https://graph.facebook.com/v2.8/me")
+
+
 
   sealed trait UserInfoClaim
 
@@ -108,40 +129,40 @@ object UserInfoEndpoint {
         import cats.std.list._
         import cats.syntax.traverse._
 
-        val xs: List[Decoder.Result[UserInfoClaim]] = obj.toList.map {
-          case ("sub", x) => x.as[String] map UserInfoClaim.Sub
-          case ("name", x) => x.as[String] map UserInfoClaim.Name
-          case ("given_name", x) => x.as[String] map UserInfoClaim.GivenName
-          case ("family_name", x) => x.as[String] map UserInfoClaim.FamilyName
-          case ("middle_name", x) => x.as[String] map UserInfoClaim.MiddleName
-          case ("nickname", x) => x.as[String] map UserInfoClaim.Nickname
-          case ("preferred_username", x) => x.as[String] map UserInfoClaim.PreferredUsername
-          case ("profile", x) => x.as[String] map UserInfoClaim.Profile
-          case ("picture", x) => x.as[String] map UserInfoClaim.Picture
-          case ("website", x) => x.as[String] map UserInfoClaim.Website
-          case ("email", x) => x.as[String] map UserInfoClaim.Email
-          case ("email_verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)) map UserInfoClaim.EmailVerified
-          case ("gender", x) => x.as[String] map UserInfoClaim.Gender
-          case ("birthdate", x) => x.as[String] map UserInfoClaim.Birthdate
-          case ("locale", x) => x.as[String] map UserInfoClaim.Locale
-          case ("phone_number", x) => x.as[String] map UserInfoClaim.PhoneNumber
-          case ("phone_number_verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)) map UserInfoClaim.PhoneNumberVerified
-          case ("address", x) => cats.data.Xor.Right(UserInfoClaim.Address(x))
-          case ("updated_at", x) => x.as[Long] map UserInfoClaim.UpdatedAt
+        val xs: List[Decoder.Result[List[UserInfoClaim]]] = obj.toList.map {
+          case ("sub", x) => x.as[String].map(UserInfoClaim.Sub).map(x => List[UserInfoClaim](x))
+          case ("name", x) => x.as[String].map(UserInfoClaim.Name).map(x => List[UserInfoClaim](x))
+          case ("given_name", x) => x.as[String].map(UserInfoClaim.GivenName).map(x => List[UserInfoClaim](x))
+          case ("family_name", x) => x.as[String].map(UserInfoClaim.FamilyName).map(x => List[UserInfoClaim](x))
+          case ("middle_name", x) => x.as[String].map(UserInfoClaim.MiddleName).map(x => List[UserInfoClaim](x))
+          case ("nickname", x) => x.as[String].map(UserInfoClaim.Nickname).map(x => List[UserInfoClaim](x))
+          case ("preferred_username", x) => x.as[String].map(UserInfoClaim.PreferredUsername).map(x => List[UserInfoClaim](x))
+          case ("profile", x) => x.as[String].map(UserInfoClaim.Profile).map(x => List[UserInfoClaim](x))
+          case ("picture", x) => x.as[String].map(UserInfoClaim.Picture).map(x => List[UserInfoClaim](x))
+          case ("website", x) => x.as[String].map(UserInfoClaim.Website).map(x => List[UserInfoClaim](x))
+          case ("email", x) => x.as[String].map(UserInfoClaim.Email).map(x => List[UserInfoClaim](x))
+          case ("email_verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)).map(UserInfoClaim.EmailVerified).map(x => List[UserInfoClaim](x))
+          case ("gender", x) => x.as[String].map(UserInfoClaim.Gender).map(x => List[UserInfoClaim](x))
+          case ("birthdate", x) => x.as[String].map(UserInfoClaim.Birthdate).map(x => List[UserInfoClaim](x))
+          case ("locale", x) => x.as[String].map(UserInfoClaim.Locale).map(x => List[UserInfoClaim](x))
+          case ("phone_number", x) => x.as[String].map(UserInfoClaim.PhoneNumber).map(x => List[UserInfoClaim](x))
+          case ("phone_number_verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)).map(UserInfoClaim.PhoneNumberVerified).map(x => List[UserInfoClaim](x))
+          case ("address", x) => cats.data.Xor.Right(List[UserInfoClaim](UserInfoClaim.Address(x)))
+          case ("updated_at", x) => x.as[Long].map(UserInfoClaim.UpdatedAt).map(x => List[UserInfoClaim](x))
 
           // facebook
-          case ("id", x) => x.as[String] map UserInfoClaim.Sub
-          case ("first_name", x) => x.as[String] map UserInfoClaim.GivenName
-          case ("last_name", x) => x.as[String] map UserInfoClaim.FamilyName
-          case ("link", x) => x.as[String] map UserInfoClaim.Profile
-          case ("timezone", x) => x.as[Int] map (x => UserInfoClaim.Zoneinfo(x.toString))
-          case ("verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)) map UserInfoClaim.EmailVerified
+          case ("id", x) => x.as[String].map(x => List[UserInfoClaim](UserInfoClaim.Sub(x), UserInfoClaim.Picture(s"https://graph.facebook.com/v2.8/$x/picture")))
+          case ("first_name", x) => x.as[String].map(UserInfoClaim.GivenName).map(x => List[UserInfoClaim](x))
+          case ("last_name", x) => x.as[String].map(UserInfoClaim.FamilyName).map(x => List[UserInfoClaim](x))
+          case ("link", x) => x.as[String].map(UserInfoClaim.Profile).map(x => List[UserInfoClaim](x))
+          case ("timezone", x) => x.as[Int] map (x => List[UserInfoClaim](UserInfoClaim.Zoneinfo(x.toString)))
+          case ("verified", x) => x.as[Boolean](Decoder[Boolean].or(strBooleanDecoder)).map(UserInfoClaim.EmailVerified).map(x => List[UserInfoClaim](x))
 
           // custom
-          case (key, x) => cats.data.Xor.Right(UserInfoClaim.Custom(key, x))
+          case (key, x) => cats.data.Xor.Right(List[UserInfoClaim](UserInfoClaim.Custom(key, x)))
         }
 
-        xs.sequence
+        xs.sequence.map(_.flatten)
 
       }
     }
@@ -194,21 +215,6 @@ object UserInfoEndpoint {
     case "google+" =>  googlePlusGetOpenIdConnectUri
   }
 
-
-  // UserInfo endpoints
-  // http://openid.net/specs/openid-connect-core-1_0.html#UserInfo
-
-  // Google: OpenID Connect
-  // https://developers.google.com/identity/protocols/OpenIDConnect
-  // https://accounts.google.com/.well-known/openid-configuration
-  val googleUserInfoUri = Uri.uri("https://www.googleapis.com/oauth2/v3/userinfo")
-
-  // Google+ API user profile in OpenID Connect-like format
-  // https://developers.google.com/+/web/api/rest/openidconnect/getOpenIdConnect
-  val googlePlusGetOpenIdConnectUri = Uri.uri("https://www.googleapis.com/plus/v1/people/me/openIdConnect")
-
-  // Facebook: proprietary OAuth 2.0 authentication extension
-  val facebookUserInfoUri = Uri.uri("https://graph.facebook.com/me")
 
 
 
@@ -279,7 +285,7 @@ object UserInfoEndpoint {
       link <- json.downField("link").as[String]
       gender <- json.downField("gender").as[String]
     } yield {
-      UserInfo(id, email, verified, name, firstName, lastName, link, f"https://graph.facebook.com/v2.3/$id/picture", gender)
+      UserInfo(id, email, verified, name, firstName, lastName, link, f"https://graph.facebook.com/v2.8/$id/picture", gender)
     }
   }
 
@@ -292,7 +298,7 @@ object UserInfoEndpoint {
       firstName <- json.downField("first_name").as[String]
       lastName <- json.downField("last_name").as[String]
     } yield {
-      BasicUserInfo(id, email, verified, name, firstName, lastName, f"https://graph.facebook.com/v2.3/$id/picture")
+      BasicUserInfo(id, email, verified, name, firstName, lastName, f"https://graph.facebook.com/v2.8/$id/picture")
     }
   }
 
